be01a5adebe5cf6a608dfea1cafe7b96313930e1
Juraj Antas <juraj.antas@gmail.com>
2015-05-18 11:38:31 +0200

    first commit

---
+++ locTest/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  locTest
+//
+//  Created by Juraj Antas on 5/14/15.
+//  Copyright (c) 2015 ixonos.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+
+
+}
+
